34522c4fb7012fef7874da667fb66dcaf0fbf2ae
Denis Golovin <denis_golovin@mail.ua>
2017-03-23 17:59:12 +0200

    almost

--- task/ListAndChuckNorris.js
+++ task/ListAndChuckNorris.js
@@ -4,25 +4,24 @@ function List(array) {
 		this.nodeName = node;
 		var newUl = document.createElement('ul');
 		var	newLi = document.createElement('li');
+		var listClone = this.list.slice();
 
-		if(node.firstElementChild === null) {
+		function makeContent() {
 			node.insertBefore(newUl, node.firstElementChild);
-			this.list.forEach((liText) => {
+			listClone.forEach((liText) => {
 				newLi.textContent = liText;
 				node.firstElementChild.appendChild(newLi.cloneNode(true));
 			})
-			return;
-		};
-
-		for(var i = 0; i <= node.children.length; i++) {
-			node.removeChild(node.children[i]);
 		}
 
-		node.insertBefore(newUl, node.firstElementChild);
-		this.list.forEach((liText) => {
-			newLi.textContent = liText;
-			node.firstElementChild.appendChild(newLi.cloneNode(true));
-		})
+		if (node.children.length === 0) {
+			makeContent();
+		} else {
+			for(var i = 0; i <= node.children.length; i++) {
+				node.removeChild(node.children[i]);
+			}
+			makeContent();
+		}
 	};
 	this.add = (liText) => {
 		var	newLi = document.createElement('li');
@@ -34,4 +33,43 @@ function List(array) {
 		this.list = newList;
 	}
 }
-var list = new List(['один', 'два', 'три']);
+var list = new List();
+
+function ChuckNorris(List, nod) {
+	this.jokes = [];
+	this.nod = nod;
+	this.callBack = (data) => {
+		console.log(data);
+	};
+	this._getJokes = function(n, fn) {
+		var req = new XMLHttpRequest();
+		var num = n;
+		if (num > 0) {
+			req.open('get', `http://api.icndb.com/jokes/random/${num}`);
+			req.addEventListener('load', () => {
+				this.callBack(JSON.parse(req.responseText).value);
+				list.list = [];
+				JSON.parse(req.responseText).value.forEach((obj) => {
+					list.list.push(obj.joke);
+				});
+			});
+		} else {
+			req.open('get', `http://api.icndb.com/jokes/random`);
+			req.addEventListener('load', () => {
+				list.list = [];
+				this.callBack(JSON.parse(req.responseText).value);
+				list.list.push(JSON.parse(req.responseText).value.joke);
+			});
+		}
+		req.send();
+	}
+	this.showJokes = function(n) {
+		this._getJokes(n);
+		list.renderTo(nod);
+	};
+	this.sayJoke = function() {
+		this._getJokes();
+		list.renderTo(nod);
+	}
+};
+var chuck = new ChuckNorris(list, document.body);
